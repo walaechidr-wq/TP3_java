9dc96893974d71690004bcb0f27885857fd42304
walae chidr <walaechidr@gmail.com>
2026-04-14 22:17:34 +0100

    NombreComplex.java

--- NombreComplex.java
+++ NombreComplex.java
@@ -1,28 +1,28 @@
 package TP3;
 
-public class NombreComplexe {
+public class NombreComplex {
     private double reel;
     private double imaginaire;
 
-    public NombreComplexe(double reel, double imaginaire) {
+    public NombreComplex(double reel, double imaginaire) {
         this.reel = reel;
         this.imaginaire = imaginaire;
     }
 
-    public NombreComplexe addition(NombreComplexe z) {
+    public NombreComplex addition(NombreComplex z) {
         double nouveauReel = this.reel + z.reel;
         double nouveauImaginaire = this.imaginaire + z.imaginaire;
-        return new NombreComplexe(nouveauReel, nouveauImaginaire);
+        return new NombreComplex(nouveauReel, nouveauImaginaire);
     }
 
-    // Méthode multiplication
-    public NombreComplexe multiplication(NombreComplexe z) {
+    
+    public NombreComplex multiplication(NombreComplex z) {
         double nouveauReel = (this.reel * z.reel) - (this.imaginaire * z.imaginaire);
         double nouveauImaginaire = (this.reel * z.imaginaire) + (this.imaginaire * z.reel);
-        return new NombreComplexe(nouveauReel, nouveauImaginaire);
+        return new NombreComplex(nouveauReel, nouveauImaginaire);
     }
 
-    // Méthode afficher
+    
     public void afficher() {
         System.out.println(reel + " + " + imaginaire + "i");
     }
